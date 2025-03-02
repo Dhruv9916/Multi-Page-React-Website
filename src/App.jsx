8436cde5decc3b9083c952cf9491a2e1441f5456
Dhruv Kumar <113141712+Dhruv9916@users.noreply.github.com>
2025-03-02 06:53:23 +0530

    Using Children Props for Layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,35 @@ import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
 import { Country } from "./pages/Country";
 import { Home } from "./pages/Home";
+import { AppLayout } from "./components/Layout/AppLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "about",
-    element: <About />,
-  },
-  {
-    path: "country",
-    element: <Country />,
-  },
-  {
-    path: "contact",
-    element: <Contact />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "country",
+        element: <Country />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
 const App = () => {
-  return <RouterProvider router={router}></RouterProvider>
+  return <RouterProvider router={router}></RouterProvider>;
 };
 
 export default App;
