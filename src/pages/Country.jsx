3f15eb929d1b81c2e2c5d07593ede3d24be894a4
Dhruv Kumar <113141712+Dhruv9916@users.noreply.github.com>
2025-03-03 00:50:44 +0530

    Creating Axios Instance and Fetching API Data with Axios

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,3 +1,39 @@
+import { useEffect, useState, useTransition } from "react";
+import { getCountryData } from "../api/postApi";
+import { Loader } from "../components/UI/Loader";
+import CountryCard from "../components/Layout/CountryCard";
+
 export const Country = () => {
-  return <h1>Country Page</h1>;
+  const [isPending, startTransition] = useTransition();
+  const [countries, setCountries] = useState([]);
+
+  //USUAL METHOD
+  // useEffect(async () => {
+  //   const res = await fetch(
+  //     "https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags"
+  //   );
+
+  //   const data = res.json();
+  //   setData(data);
+  // }, []);
+
+  useEffect(() => {
+    startTransition(async () => {
+      const res = await getCountryData();
+      // console.log(res);
+      setCountries(res.data);
+    });
+  }, []);
+
+  if (isPending) return <Loader />;
+
+  return (
+    <section className="country-section">
+      <ul className="grid grid-four-cols">
+        {countries.map((curCountry) => {
+          return <CountryCard country={curCountry} key={curCountry.id} />;
+        })}
+      </ul>
+    </section>
+  );
 };
