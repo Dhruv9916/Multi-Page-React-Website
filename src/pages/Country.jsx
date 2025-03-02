8c77f575af23dd43c2c5f9ad78b31a12d45eff11
Dhruv Kumar <113141712+Dhruv9916@users.noreply.github.com>
2025-03-03 04:03:30 +0530

    Implementing Search & Filter Logic

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -2,10 +2,14 @@ import { useEffect, useState, useTransition } from "react";
 import { getCountryData } from "../api/postApi";
 import { Loader } from "../components/UI/Loader";
 import CountryCard from "../components/Layout/CountryCard";
+import SearchFilter from "../components/UI/SearchFilter";
 
 export const Country = () => {
   const [isPending, startTransition] = useTransition();
   const [countries, setCountries] = useState([]);
+
+  const [search, setSearch] = useState();
+  const [filter, setFilter] = useState("all");
 
   //USUAL METHOD
   // useEffect(async () => {
@@ -27,10 +31,36 @@ export const Country = () => {
 
   if (isPending) return <Loader />;
 
+  //console.log(search, filter);
+
+  //Logic for Search Functionality and Filter Functionality
+
+  const searchCountry = (country) => {
+    if (search) {
+      return country.name.common.toLowerCase().includes(search.toLowerCase());
+    }
+    return country;
+  };
+
+  const filterRegion = (country) => {
+    if (filter === "all") return country;
+    return country.region === filter;
+  };
+
+  const filterCountries = countries.filter(
+    (country) => searchCountry(country) && filterRegion(country)
+  );
+
   return (
     <section className="country-section">
+      <SearchFilter
+        search={search}
+        setSearch={setSearch}
+        filter={filter}
+        setFilter={setFilter}
+      />
       <ul className="grid grid-four-cols">
-        {countries.map((curCountry) => {
+        {filterCountries.map((curCountry) => {
           return <CountryCard country={curCountry} key={curCountry.id} />;
         })}
       </ul>
